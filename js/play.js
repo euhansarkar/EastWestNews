0bdbec0ca295edd0eb39dc039ab0f5eb50bd895a
euhansarkar <euhansarkar@gmail.com>
2022-09-03 01:51:59 +0600

    basic webpage completed

--- js/play.js
+++ js/play.js
@@ -11,13 +11,13 @@ const displayCatagories = catagories => {
         let createDiv = document.createElement(`div`);
         createDiv.classList.add(`col`);
         createDiv.innerHTML = `
-            <a class="fs-6" onclick="loadOpenCatagory('${catagory.category_id}')">${catagory.category_name}</a>
+            <a class="fs-6 text-dark" onclick="loadOpenCatagory('${catagory.category_id}')">${catagory.category_name}</a>
         `
         getCatagories.appendChild(createDiv);
     })
 }
 
-const loadOpenCatagory = async(catagoryId) => {
+const loadOpenCatagory = async(catagoryId = `01`) => {
     let url = `https://openapi.programming-hero.com/api/news/category/${catagoryId}`;
     let res = await fetch(url);
     let data = await res.json();
@@ -31,7 +31,7 @@ const displayOpenCatagory = async(catagoryDetails) => {
         let createNewDiv = document.createElement(`div`);
         createNewDiv.classList.add(`col`);
         createNewDiv.innerHTML = `
-            <div class="card mb-3 w-100 mx-5">
+            <div class="card mb-3 mx-auto p-3" style="width: 1000px">
                 <div class="row g-0">
                 <div class="col-md-4">
                     <img src="${catagoryItem.image_url}" class="img-fluid rounded-start" alt="...">
@@ -40,7 +40,20 @@ const displayOpenCatagory = async(catagoryDetails) => {
                     <div class="card-body">
                     <h5 class="card-title">${catagoryItem.title}</h5>
                     <p class="card-text">${catagoryItem.details.slice(0, 200)}</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div class="d-flex justify-content-between align-items-center mt-3">
+                        <div class="author d-flex">
+                            <div class="author-img" style="margin-right: 15px"><img src="${catagoryItem.author.img}" class="img-fluid rounded" alt""></div>
+                            <div>
+                                <p style="font-size: 13px; font-weight: 600; margin: 0">${catagoryItem.author.name}</p>
+                                <p style="font-size: 12px; font-weight: 500; margin: 0">${catagoryItem.author.published_date}</p>
+                            </div>
+                        </div>
+                        <div class=""><i class="fa-solid fa-eye"></i>  ${catagoryItem.total_view}</div>
+                        <div class="">rating: ${catagoryItem.rating.number}</div>
+                        <div class="bg-dark text-light rounded p-1">
+                            <a>show more <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
                     </div>
                 </div>
                 </div>
@@ -48,7 +61,7 @@ const displayOpenCatagory = async(catagoryDetails) => {
         `
         getCatagoriesItemsContainer.appendChild(createNewDiv);
     })
-    console.log(catagoryDetails);
 }
 
 loadCatagories();
+loadOpenCatagory();
